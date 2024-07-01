e8f7449a968382f00f3dbe6842bd745096b5c5c6
olegspliska <olegpliska1@gmail.com>
2024-07-01 12:50:35 +0300

    edit main activity modifier size

--- app/src/main/java/com/opliska/f1calendarapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/presentation/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.opliska.f1calendarapplication.ui.theme.F1Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,7 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             F1Theme {
-                MainScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
