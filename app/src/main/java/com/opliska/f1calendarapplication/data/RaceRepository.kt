985b210a19b22e916c10a5ddc8651fd16db95a93
olegspliska <olegpliska1@gmail.com>
2024-07-01 20:38:55 +0300

    init repository with firebase call

--- app/src/main/java/com/opliska/f1calendarapplication/data/RaceRepository.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/data/RaceRepository.kt
@@ -1,27 +1,39 @@
 package com.opliska.f1calendarapplication.data
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.opliska.f1calendarapplication.data.model.Race
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class RaceRepository @Inject constructor() {
 
-    fun getAll(): Flow<RequestResult<List<Race>>> {
-        val mockData = listOf(
-            Race(
-                round = "1",
-                raceName = "Canadian Grand Prix",
-                date = "25-27 MARCH"),
-            Race(
-                round = "2",
-                raceName = "Australian Grand Prix",
-                date = "14-16 APRIL"),
-            Race(
-                round = "3",
-                raceName = "Estonian Grand Prix",
-                date = "16-18 JUNE"),
-        )
-        return flowOf(RequestResult.Success(mockData))
+    private val database = Firebase.database
+    private val databaseReference: DatabaseReference = database.getReference("races2024")
+
+    fun getAll(): Flow<RequestResult<List<Race>>> = callbackFlow {
+
+        val racesListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val racesList = snapshot.children.mapNotNull { it.getValue(Race::class.java) }
+                trySend(RequestResult.Success(racesList)).isSuccess
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                trySend(RequestResult.Error(error = Exception(error.message))).isSuccess
+            }
+        }
+        databaseReference.addValueEventListener(racesListener)
+        awaitClose { databaseReference.removeEventListener(racesListener) }
+    }.catch { e ->
+        emit(RequestResult.Error(error = e))
     }
 }
+
