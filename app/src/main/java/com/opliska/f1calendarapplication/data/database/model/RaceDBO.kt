426119691b09b4029442683fd9dc2f8f46599655
olegspliska <olegpliska1@gmail.com>
2024-07-04 11:57:55 +0300

    modify RaceDBO class

--- app/src/main/java/com/opliska/f1calendarapplication/data/database/model/RaceDBO.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/data/database/model/RaceDBO.kt
@@ -4,22 +4,62 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.Date
+import com.opliska.f1calendarapplication.data.model.Location
 
 @Entity(tableName = "races")
 data class RaceDBO(
-    @Embedded(prefix = "source") val source: Source?,
-    @ColumnInfo("author") val author: String?,
-    @ColumnInfo("title") val title: String?,
-    @ColumnInfo("description") val description: String?,
+    @Embedded(prefix = "circuit") val circuit: CircuitDBO?,
+    @Embedded(prefix = "first_practice") val firstPractice: FirstPracticeDBO?,
+    @Embedded(prefix = "second_practice") val secondPractice: SecondPracticeDBO?,
+    @Embedded(prefix = "third_practice") val thirdPractice: ThirdPracticeDBO?,
+    @Embedded(prefix = "qualifying") val qualifying: QualifyingDBO?,
+    @Embedded(prefix = "sprint") val sprint: SprintDBO?,
+    @Embedded(prefix = "lap_record") val lapRecord: LapRecordDBO?,
+    @ColumnInfo("date") val date: String?,
+    @ColumnInfo("race_name") val raceName: String?,
+    @ColumnInfo("round") val round: String?,
+    @ColumnInfo("season") val season: String?,
+    @ColumnInfo("time") val time: String?,
+    @ColumnInfo("trackLength") val trackLength: Int?,
     @ColumnInfo("url") val url: String?,
-    @ColumnInfo("urlToImage") val urlToImage: String?,
-    @ColumnInfo("publishedAt") val publishedAt: Date?,
-    @ColumnInfo("content") val content: String?,
+    @ColumnInfo("yearStarted") val yearStarted: String?,
     @PrimaryKey(autoGenerate = true) val id: Long = 0
 )
 
-data class Source(
-    val id: String,
-    val name: String
+data class CircuitDBO(
+    val location: Location? = null,
+    val circuitId: String? = null,
+    val circuitName: String? = null,
+    val url: String? = null
+)
+
+data class LapRecordDBO(
+    val driver: String? = null,
+    val time: String? = null,
+    val year: String? = null
+)
+
+data class FirstPracticeDBO(
+    val date: String? = null,
+    val time: String? = null
+)
+
+data class SecondPracticeDBO(
+    val date: String? = null,
+    val time: String? = null
+)
+
+data class ThirdPracticeDBO(
+    val date: String? = null,
+    val time: String? = null
+)
+
+data class QualifyingDBO(
+    val date: String? = null,
+    val time: String? = null
+)
+
+data class SprintDBO(
+    val date: String? = null,
+    val time: String? = null
 )
