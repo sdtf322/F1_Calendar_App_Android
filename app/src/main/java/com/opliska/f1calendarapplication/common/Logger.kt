8e3f028938f6714d46a34014bd417b3d239a39c3
olegspliska <olegpliska1@gmail.com>
2024-07-04 18:22:49 +0300

    add Logger

---
+++ app/src/main/java/com/opliska/f1calendarapplication/common/Logger.kt
@@ -0,0 +1,32 @@
+package com.opliska.f1calendarapplication.common
+
+import android.util.Log
+
+public interface Logger {
+    public fun d(
+        tag: String,
+        message: String
+    )
+
+    public fun e(
+        tag: String,
+        message: String
+    )
+}
+
+public fun AndroidLogcatLogger(): Logger =
+    object : Logger {
+        override fun d(
+            tag: String,
+            message: String
+        ) {
+            Log.d(tag, message)
+        }
+
+        override fun e(
+            tag: String,
+            message: String
+        ) {
+            Log.e(tag, message)
+        }
+    }
