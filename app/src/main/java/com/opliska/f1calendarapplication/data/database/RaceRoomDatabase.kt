bcf62eacf0db116c8071fe2f189d7a87a2519d0f
olegspliska <olegpliska1@gmail.com>
2024-07-04 12:09:42 +0300

    set correct RaceRoomDatabase initialization

--- app/src/main/java/com/opliska/f1calendarapplication/data/database/RaceRoomDatabase.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/data/database/RaceRoomDatabase.kt
@@ -4,27 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import com.opliska.f1calendarapplication.data.database.dao.RaceDao
+import com.opliska.f1calendarapplication.data.database.model.RaceDBO
 
-class F1RoomDatabase {
+class RaceDatabase internal constructor(private val database: F1RoomDatabase) {
+    val racesDao: RaceDao
+        get() = database.racesDao()
 }
 
-//class NewsDatabase internal constructor(private val database: NewsRoomDatabase) {
-//    val articlesDao: ArticleDao
-//        get() = database.articlesDao()
-//}
-//
-//@Database(entities = [ArticleDBO::class], version = 1)
-//internal abstract class NewsRoomDatabase : RoomDatabase() {
-//    abstract fun articlesDao(): ArticleDao
-//}
-//
-//fun NewsDatabase(applicationContext: Context): NewsDatabase {
-//    val newsRoomDatabase =
-//        Room.databaseBuilder(
-//            checkNotNull(applicationContext.applicationContext),
-//            NewsRoomDatabase::class.java,
-//            "news"
-//        ).build()
-//    return NewsDatabase(newsRoomDatabase)
-//}
+@Database(entities = [RaceDBO::class], version = 1)
+internal abstract class F1RoomDatabase : RoomDatabase() {
+    abstract fun racesDao(): RaceDao
+}
+
+fun RaceDatabase(applicationContext: Context): RaceDatabase {
+    val newsRoomDatabase =
+        Room.databaseBuilder(
+            checkNotNull(applicationContext.applicationContext),
+            F1RoomDatabase::class.java,
+            "news"
+        ).build()
+    return RaceDatabase(newsRoomDatabase)
+}
