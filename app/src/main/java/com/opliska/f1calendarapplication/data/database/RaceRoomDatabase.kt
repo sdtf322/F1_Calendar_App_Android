990a94ad173cdea7a10732af470a11edbd235d94
olegspliska <olegpliska1@gmail.com>
2024-07-05 13:36:21 +0300

    cleanup

--- app/src/main/java/com/opliska/f1calendarapplication/data/database/RaceRoomDatabase.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/data/database/RaceRoomDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.opliska.f1calendarapplication.data.database.dao.RaceDao
 import com.opliska.f1calendarapplication.data.database.model.RaceDBO
 
