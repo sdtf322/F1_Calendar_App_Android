394c01cac5044dd66ced502243d9bc32fe905144
olegspliska <olegpliska1@gmail.com>
2024-07-06 12:17:28 +0300

    fix font issue

--- app/src/main/java/com/opliska/f1calendarapplication/ui/theme/F1Typography.kt
+++ app/src/main/java/com/opliska/f1calendarapplication/ui/theme/F1Typography.kt
@@ -4,6 +4,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.opliska.f1calendarapplication.R
 
 data class F1Typography(
     val title1: TextStyle = Title1,
@@ -11,12 +14,22 @@ data class F1Typography(
     val defaultTypography: Typography = Typography
 )
 
+val SplineSans = FontFamily(
+    Font(R.font.spline_sans_light, FontWeight.Light),
+    Font(R.font.spline_sans_regular, FontWeight.Normal),
+    Font(R.font.spline_sans_medium, FontWeight.Medium),
+    Font(R.font.spline_sans_semi_bold, FontWeight.SemiBold),
+    Font(R.font.spline_sans_bold, FontWeight.Bold)
+)
+
 val Title1 = TextStyle(
+    fontFamily = SplineSans,
     fontWeight = FontWeight.Bold,
     fontSize = 24.sp
 )
 
 val CustomBody1 = TextStyle(
+    fontFamily = SplineSans,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp
 )
